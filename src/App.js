3859dc4bad918ff9cf6120645fe3abc4982b4db5
saiteja <87071267+mukkasaiteja@users.noreply.github.com>
2023-04-21 09:10:47 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="img">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0rew0mY4ZpVitWH3sKw9eRaEuZJWdGnNFAg&usqp=CAU" />
+      </div>
+      <h3>saiteja</h3>
+      <div className="socialmedia">
+        <section>
+          <p className="number">26</p>
+          <p>shoot</p>
+        </section>
+        <section>
+          <p className="number">94</p>
+          <p>Followers</p>
+        </section>
+        <section>
+          <p className="number">139</p>
+          <p>following</p>
+        </section>
+      </div>
+    </div>
+  );
+}
